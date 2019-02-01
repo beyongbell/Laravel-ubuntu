571f33bfe66d3d9f53c971ad6c51d494e38f3f99
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-02-01 23:25:54 +0700

    One Comit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    P'BelL Love N'Earn
                 </div>
 
                 <div class="links">
